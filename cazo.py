efcb3fd21b8b19ba3778fe9e2b744d751498e357
jartigag <jartigag@protonmail.com>
2018-03-05 23:16:15 +0100

    cazo: greater-than, less-than args

--- cazo.py
+++ cazo.py
@@ -27,7 +27,48 @@ from tenedor import basics, over_time
 
 __version__ = '0.1'
 
-from secrets1 import consumer_key, consumer_secret, access_token, access_token_secret
+from secrets4 import consumer_key, consumer_secret, access_token, access_token_secret
+
+def checkBasics(n_tweets, l_ratio, n_followers, f_ratio):
+	if args.tweets:
+		if args.tweets > 0:
+			if n_tweets < args.tweets: return False
+		if args.tweets < 0:
+			if n_tweets > -args.tweets: return False
+
+	if args.likes_tweets_ratio:
+		if args.likes_tweets_ratio > 0:
+			if l_ratio < args.likes_tweets_ratio: return False
+		if args.likes_tweets_ratio < 0:
+			if l_ratio > -args.likes_tweets_ratio: return False
+
+	if args.followers:
+		if args.followers > 0:
+			if n_followers < args.followers: return False
+		if args.followers < 0:
+			if n_followers > -args.followers: return False
+
+	if args.followers_following_ratio:
+		if args.followers_following_ratio > 0:
+			if f_ratio < args.followers_following_ratio: return False
+		if args.followers_following_ratio < 0:
+			if f_ratio > -args.followers_following_ratio: return False
+
+	return True
+
+def checkOverTime(tweets_day_avg, retweets_percent):
+	if args.tweets_day_average:
+		if args.tweets_day_average > 0:
+			if tweets_day_avg < args.tweets_day_average: return False
+		if args.tweets_day_average < 0:
+			if tweets_day_avg > -args.tweets_day_average: return False
+	if args.retweets_percent:
+		if args.retweets_percent > 0:
+			if retweets_percent < args.retweets_percent: return False
+		if args.retweets_percent < 0:
+			if retweets_percent > -args.retweets_percent: return False
+
+	return True
 
 def main():
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -40,59 +81,59 @@ def main():
 	todayslist = 'cazo-'+datetime.now().strftime('%y-%m-%d_%H:%M')
 	list = api.create_list(todayslist,'private')
 
-	t = 4 # secs between tries
+	t = 120 # secs between tries
 	n = 0
 	while True:
 		try:
+
 			randFlwr = api.followers_ids(screen_name=myUsername)[randrange(myCount)]
 			randFlwrUsername = api.get_user(randFlwr).screen_name
 			randFlwrCount = api.get_user(randFlwr).followers_count
 			randFlwrOfFlwr = api.followers_ids(screen_name=randFlwrUsername)[randrange(randFlwrCount)]
 			randFlwrOfFlwrUsername = api.get_user(randFlwrOfFlwr).screen_name
+
 			n+=1
+			
 			print("(%i) [sleep %i, reqs left: %s flwrs, %s tweets]" % (n,t,api.rate_limit_status()['resources']['followers']['/followers/list']['remaining'],api.rate_limit_status()['resources']['statuses']['/statuses/user_timeline']['remaining']))
+			
 			if randFlwrOfFlwrUsername==myUsername: continue
 			
 			n_tweets, l_ratio, n_followers, f_ratio = basics(api, randFlwrOfFlwrUsername)
 			
-			if args.tweets:
-				if n_tweets > args.tweets: continue
-			if args.likes_tweets_ratio:
-				if l_ratio > args.likes_tweets_ratio: continue
-			if args.followers:
-				if n_followers > args.followers: continue
-			if args.followers_following_ratio:
-				if f_ratio > args.followers_following_ratio: continue
+			if not checkBasics(n_tweets, l_ratio, n_followers, f_ratio): continue
 			
 			n_days, start_date, end_date, num_tweets, tweets_day_avg, retweets_percent = over_time(api, randFlwrOfFlwrUsername)
 			
-			if args.tweets_day_average:
-				if tweets_day_avg < args.tweets_day_average: continue
-			if args.retweets_percent:
-				if retweets_percent < args.retweets_percent: continue
+			if not checkOverTime(tweets_day_avg, retweets_percent): continue
 			
 			print("    \033[1m%s\033[0m (%.2f fwrs/fwng, %.2f tweets/day)" % (randFlwrOfFlwrUsername,f_ratio,tweets_day_avg))
 			api.add_list_member(list_id=list.id,owner_screen_name='@'+myUsername,id=randFlwrOfFlwr)
 		
 			sleep(t)
-		
+
 		except tweepy.error.RateLimitError as e:
 			print('    %s /followers/list requests left' % api.rate_limit_status()['resources']['followers']['/followers/list']['remaining'])
 			print('    %s /statuses/user_timeline left '% api.rate_limit_status()['resources']['statuses']['/statuses/user_timeline']['remaining'])
+			
 			reset_time = api.rate_limit_status()['resources']['followers']['/followers/list']['reset']
 			current_time = int(time())
 			wait_time = reset_time - current_time
+			
 			print(" == %i fetched with %i-secs pauses (sleeping %i secs)" % (n,t,wait_time))
+			
 			n=0
+			
 			#TODO: progressbar instead of this while
 			while current_time<reset_time:
 				current_time = int(time())
 				wait_time = reset_time - current_time
 				sleep(60)
 				print("sleeping %i secs more.. (=%i minutes)" % (wait_time,wait_time/60))
+
 		except Exception as e:
 			print(e)
 			pass
+
 		sleep(t)
 
 	#TODO: guardar los ids ya analizados (primero en array, más adelante en db)
@@ -102,11 +143,9 @@ def main():
 
 if __name__ == '__main__':
 
-	#TODO: greater-than, less-than args
-
 	parser = argparse.ArgumentParser(description=
 		">>\"serve from pot to dish\" - tool for twitter, version %s by @jartigag" % __version__,
-									 usage='%(prog)s [options]')
+									 usage='%(prog)s [arguments (+value: >value, -value: <value)]')
 	parser.add_argument('-f', '--followers', type=int,
 						help='filter by number of followers')
 	parser.add_argument('-l', '--likes_tweets_ratio', type=float,
@@ -126,4 +165,5 @@ if __name__ == '__main__':
 	    print("[!] set some parameters to filter users!")
 	    parser.print_help()
 	else:
+		#print(vars(args).values())
 		main()
